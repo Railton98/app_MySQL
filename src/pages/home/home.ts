3750b3bf4cdbf82aaeb6b1139aa6356d76060540
Rayltton's Teck <railtonleal98@gmail.com>
2018-04-21 00:42:52 -0300

    Adicionando ao Versionamento

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { UsuarioProvider } from './../../providers/usuario/usuario';
+import { PerfilProvider } from './../../providers/perfil/perfil';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers:[UsuarioProvider, PerfilProvider]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public usuarios = [];
+  public perfis = [];
 
+  public usuarioCadastro = {"id": "", "nome": "", "idade": null};
+
+  constructor(public navCtrl: NavController,
+    private usuarioService: UsuarioProvider,
+    private perfilProvider: PerfilProvider) {
+    this.getUsuarios();
+    this.getPerfis();
+  }
+
+  public getUsuarios(){
+    this.usuarioService.findAll().subscribe(response => this.usuarios = response);
+  }
+
+  public getPerfis(){
+    this.perfilProvider.findAll()
+    .subscribe(response => this.perfis = response);
+  }
+
+  public popularForm(usuario){
+    this.usuarioCadastro = usuario;
+  }
+
+  public compareFn(e1: any, e2: any): boolean {
+  return e1 && e2 ? e1.id === e2.id : e1 === e2;
+}
+
+  public salvarUsuario(){
+
+    if(this.usuarioCadastro.id == ""){
+      this.usuarioService.salvar(this.usuarioCadastro).subscribe(response => this.getUsuarios());
+    } else{
+      this.usuarioService.editar(this.usuarioCadastro).subscribe(response => this.getUsuarios());
+    }
+  }
+
+  public deletar(id){
+    this.usuarioService.deletar(id).subscribe(response => this.getUsuarios());
   }
 
 }
